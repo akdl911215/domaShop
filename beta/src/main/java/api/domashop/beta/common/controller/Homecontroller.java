0358c780a78d96d510c5614a820e2c2f20e5a94c
LEE Jung Hyun <akdl911215@gmail.com>
2021-06-27 15:38:27 +0900

    기초셋팅

--- beta/src/main/java/api/domashop/beta/common/controller/Homecontroller.java
+++ beta/src/main/java/api/domashop/beta/common/controller/Homecontroller.java
@@ -1,5 +1,22 @@
 package api.domashop.beta.common.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
 public class Homecontroller {
+
+    @GetMapping("/")
+    public String home() {
+        return new SimpleDateFormat("yyyy-MM-dd !!!").format(new Date());
+    }
+
+    @GetMapping("/time")
+    public String timeCount() {
+        Date date_now = new Date(System.currentTimeMillis());
+        return "현재 시간 : " + date_now;
+    }
 }
